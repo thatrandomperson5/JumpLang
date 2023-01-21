8b76280f224ce93eff9ebace48d66461e4ee1981
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2023-01-21 22:16:28 +0000

    More tests

--- site/jsite.nim
+++ site/jsite.nim
@@ -1,15 +1,15 @@
-import libjumplang/[ast, interpreter, parser, syms]
+# import libjumplang/[ast, interpreter, parser, syms]
 include karax/prelude 
 
-proc interpret(f: kstring): kstring =
-  try:
-    let a = parse($(f))
-    a.ensureSemantics()
-    let res = a.interpret("<Unnamed>")
-    if res.failed:
-      result.add res.msg
-  except CatchableError as e:
-    result.add e.msg
+# proc interpret(f: kstring): kstring =
+#   try:
+#     let a = parse($(f))
+#     a.ensureSemantics()
+#     let res = a.interpret("<Unnamed>")
+#     if res.failed:
+#       result.add res.msg
+#   except CatchableError as e:
+#     result.add e.msg
 
 proc createDom(): VNode = 
   buildHtml(tdiv):
@@ -18,8 +18,9 @@ proc createDom(): VNode =
     button:
       text "Run"
       proc onclick(ev: Event; n: VNode) =
-        getVNodeById("output").text = "Working..."
-        let res = interpret(getVNodeById("input").text)
-        getVNodeById("output").text = res
+        getVNodeById("output").text = "Working...\n"
+        # let res = interpret(getVNodeById("input").text)
+        # getVNodeById("output").add text res
+        getVNodeById("output").add text "Finished"
 
 setRenderer createDom
