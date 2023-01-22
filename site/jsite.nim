463cbd381d671e5125415cb1bfc439bd2d5d4ff3
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2023-01-22 23:56:10 +0000

    Themeing

--- site/jsite.nim
+++ site/jsite.nim
@@ -23,13 +23,19 @@ var myCodeMirror: CodeMirror
 proc newCodeMirror(element: Element, config: js): CodeMirror {. importcpp: "CodeMirror(@)" .}
 proc setValue(cm: CodeMirror, value: kstring) {.importcpp: "#.setValue(@)".}
 proc getValue(cm: CodeMirror): kstring {.importcpp: "#.getValue()".}
+proc isDarkMode(): bool {.importcpp: "isDarkMode()".}
 
 proc postRender() =
+  var theTheme = "default".kstring
+  if isDarkMode():
+    theTheme = "dracula".kstring
+  
   if myCodeMirror.Element == nil:
     myCodeMirror = newCodeMirror(kdom.getElementById("input"), js{
-      mode: "text/python".kstring,
+      mode: "python".kstring,
       value: "".kstring,
       lineNumbers: true,
+      theme: theTheme
     })
 
 let boxStyle = style(
@@ -38,8 +44,8 @@ let boxStyle = style(
   (StyleAttr.borderStyle, "solid".kstring),
   (StyleAttr.borderWidth, "1px".kstring),
   (StyleAttr.borderColor, "--highlight".kstring),
-  (StyleAttr.maxHeight, "50vh"),
-  (StyleAttr.overflow, "scroll")
+  (StyleAttr.maxHeight, "50vh".kstring),
+  (StyleAttr.overflow, "scroll".kstring)
 )
 
 proc fillExample(ev: Event, n: VNode) =
@@ -65,18 +71,19 @@ proc createDom(): VNode =
     pre(style=boxStyle):
       code:
         text o
-    button:
-      text "Run"
-      proc onclick(ev: Event; n: VNode) =
-        o = "Working...\n"
-        let code = myCodeMirror.getValue()
-        if code != "".kstring:
-          let res = interpret(code)
-          o.add res
+    tdiv(style=style(StyleAttr.display, "flex")):
+      button:
+        text "Run"
+        proc onclick(ev: Event; n: VNode) =
+          o = "Working...\n"
+          let code = myCodeMirror.getValue()
+          if code != "".kstring:
+            let res = interpret(code)
+            o.add res
           o.add "Finished"
-    select(name="Examples", onchange=fillExample):
-      for name in exampleOpts:
-        option: text name
+      select(name="Examples", onchange=fillExample):
+        for name in exampleOpts:
+          option: text name
         
     
 
---
+++ site/utils.js
@@ -0,0 +1,6 @@
+function isDarkMode() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return true;
+  }
+  return false;
+}
