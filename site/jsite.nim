3ea07aad8ce9cc78cfa1d0a3c36f28dfab8a2c59
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2023-01-22 17:52:40 +0000

    $

--- site/jsite.nim
+++ site/jsite.nim
@@ -43,7 +43,7 @@ let boxStyle = style(
 proc fillExample(ev: Event, n: VNode) =
   var txt: kstring
   let sel = n.value
-  case sel.string
+  case $sel
   of "Hello World":
     txt = staticRead("../tests/test.jmp").kstring
   of "Loops":
