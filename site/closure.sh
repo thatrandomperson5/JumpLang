6985bef7dcdd02d16860a984e499adcc6579abb5
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2023-02-03 08:36:09 -0600

    Update closure.sh

--- site/closure.sh
+++ site/closure.sh
@@ -5,7 +5,7 @@ then
     curl -o compiler.jar "https://repo1.maven.org/maven2/com/google/javascript/closure-compiler/v20230103/closure-compiler-v20230103.jar"
   fi
   echo "Running closure:"
-  java -jar ./compiler.jar --js ./app.js --js_output_file ./release.js
+  java -jar ./compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ./app.js --js_output_file ./release.js
   rm ./app.js
 else
   mv ./app.js ./release.js
